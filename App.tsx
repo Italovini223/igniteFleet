d7c232acd63411c986024b6103eedc6d512bee82
Italovini223 <italovinicius2018@gmail.com>
2023-08-26 16:55:29 -0300

    💄 add Loading component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
 import { ThemeProvider } from 'styled-components/native';
 
 import theme from './src/theme';
+
 import { SingIn } from './src/screens/SingIn';
+import { Loading } from './src/componentes/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded){
+    return(
+      <Loading />
+    )
+  }
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
       <SingIn />
     </ThemeProvider>
   );
